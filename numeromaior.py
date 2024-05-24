778d0f9a660660fd419223eb44186a3405bb7fa3
Lucas Marques <lucasmarq1410@gmail.com>
2024-05-24 15:31:50 -0300

    pronto

---
+++ numeromaior.py
@@ -0,0 +1,10 @@
+num1 = float(input("digite um numero: "))
+num2 = float(input("digite outro numero: "))
+if  num1 > num2:
+    print (num1)
+elif  num2 > num2:
+    print (num2)
+elif num1 == num2:
+    print ("numeros iguais")
+else:
+    print("erro")
