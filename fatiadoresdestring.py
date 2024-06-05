7f0499a4f02c51a11e7d49c2757a6226500cbe5f
Lucas Marques de Oliveira <167311748+lucasmarqoliv@users.noreply.github.com>
2024-06-05 16:26:17 -0300

    up

---
+++ fatiadoresdestring.py
@@ -0,0 +1,6 @@
+nome = str(input('digite seu nome: '))
+nome_sem_espacos = nome.replace(' ', '')
+print (f'seu nome com letras maiusculas: {nome.upper()}')
+print (f'seu nome com letras minusculas: {nome.lower()}')
+print (f'quantidade de letras sem considerar espaços: {nome_sem_espacos.__len__()}')
+print (f'quantidade de letras tem o primeiro nome: {nome.find(' ')}')
