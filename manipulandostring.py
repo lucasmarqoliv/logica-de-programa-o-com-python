9d6ac829edb9c4fdc95d2631a789738c87fadafc
Lucas Marques de Oliveira <167311748+lucasmarqoliv@users.noreply.github.com>
2024-06-08 11:42:26 -0300

    up

--- manipulandostring.py
+++ manipulandostring.py
@@ -1,7 +1,7 @@
 # verificar se nome da cidade começa com 'SANTO'
 cidade = str(input('digite sua cidade: ')).strip().upper()
-nome_cidade = cidade.startswith('SANTO')
-if nome_cidade == True:
+cidade = cidade.startswith('SANTO')
+if cidade == True:
     print ('sua cidade começa com santo.')
 else:
     print ('sua cidade não começa com santo.')
